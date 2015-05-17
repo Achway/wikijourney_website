a4d3af27406356fa06e0e2d2cf80c5e072266f6b
Thomas Gaudin <thomas.gaudin@centraliens-lille.org>
2015-05-17 18:20:18 +0200

    Reorganized team description

--- equipe.php
+++ equipe.php
@@ -6,8 +6,14 @@
 
 <p>Ce site web est encore en construction, merci de revenir plus tard ! ;)</p>
 <!-- faudra reprendre des photos -->
-<!-- Juliette Maës
-Depuis toujours intéressée par l’informatique, j’ai rejoint WikiJourney pour développer mes connaissances et surtout participer à un projet au concept primordial. Visitant de nouvelles villes plusieurs fois par an, j’ai pu remarquer que les guides touristiques ne sont pas toujours pratiques. Les informations sont librement disponibles sur internet, mais pas facilement accessibles aux touristes. -->
+<!-- <p>Juliette Maës <br/>
+Depuis toujours intéressée par l’informatique, j’ai rejoint WikiJourney pour 
+développer mes connaissances et surtout participer à un projet au concept 
+primordial. Visitant de nouvelles villes plusieurs fois par an, j’ai pu remarquer 
+que les guides touristiques ne sont pas toujours pratiques. Les informations sont 
+librement disponibles sur internet, mais pas facilement accessibles aux touristes.</p> -->
+<!-- <p>Thomas Gaudin <br/>
+En fait non, j'ai pas d'idée... </p> -->
 
 <?php
 	include("./include/bas.php");
